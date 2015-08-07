eb34c718a22458e1891809cfe62fa60b12a6c4a4
Andrea <andrea.peruffo1982@gmail.com>
2015-08-07 17:21:24 +0200

    first version to be applyed on Akka.js

--- main/src/main/scala/demo/Demo3.scala
+++ main/src/main/scala/demo/Demo3.scala
@@ -5,11 +5,22 @@ package demo.unicredit
 import scala.scalajs.js.JSApp
 
 import scala.scalajs.js
-import js.annotation.JSExport
+import js.annotation._
 
-@JSExport case class Foo2(i: Int, s: String)
+@JSExport case class Foo2 (i: Int, s: String) {
+	def fooDef() = "s is "+s+" i is "+i
+
+	val fooField= (for (k <- 0 until i) yield s).mkString("[",",","]")
+}
 case class Foo3(i: Int, s: String)
 case class Foo4(i: Int, s: String)
+
+@JSExportDescendentClasses(ignoreInvalidDescendants = true)
+abstract class AbstractFoo
+
+	case class Foo6(i: Int, s: String) extends AbstractFoo
+
+	protected[unicredit] case class Foo5(i: Int, s: String) extends AbstractFoo
 
 object Demo2 extends JSApp {
 
--- plugin/src/main/scala/unicredit/AnnotationAdderPlugin.scala
+++ plugin/src/main/scala/unicredit/AnnotationAdderPlugin.scala
@@ -11,7 +11,8 @@ import java.nio.file.Files.readAllBytes
 import java.nio.file.Paths.get
 
 import scala.collection.mutable
-import scala.util.{Try => STry, Success, Failure}
+import scala.util.{ Try => STry, Success, Failure }
+import scala.reflect.internal.MissingRequirementError
 
 class AnnotationAdderPlugin(val global: Global) extends Plugin {
   import global._
@@ -20,46 +21,100 @@ class AnnotationAdderPlugin(val global: Global) extends Plugin {
   val description = "Want to add annotation to classes, fields, and methods"
   val components = List[PluginComponent](AnnotationAdderComponent)
 
-  lazy val config: mutable.Set[(String, String)] = 
+  lazy val config: mutable.Set[(String, String, List[String])] =
     (try new String(readAllBytes(get("./annotation_adder.config"))).split("\n").toSeq.map(e => {
-       val splitted = e.split(" ")
-       (splitted(0), splitted(1))
+      val splitted = e.split(" ")
+      (splitted(0), splitted(1), splitted.drop(2).toList)
     })
-     catch {
-       case err: Throwable =>
-         println("Annotation adder configuration file is missing")
-         Seq()
-     }).to[mutable.Set]
+    catch {
+      case err: Throwable =>
+        println("Annotation adder configuration file is missing")
+        Seq()
+    }).to[mutable.Set]
 
-  private object AnnotationAdderComponent extends PluginComponent {
+  private object AnnotationAdderComponent extends PluginComponent with Transform {
     val global = AnnotationAdderPlugin.this.global
     import global._
+    import global.definitions._
 
     override val runsAfter = List("typer")
 
     val phaseName = "annotation-adder"
 
-    override def newPhase(prev: Phase): StdPhase = new StdPhase(prev) {
-      override def apply(unit: CompilationUnit) {
-        
-        config.foreach { (c: (String, String)) =>
-          val clazz = STry {
-            rootMirror.getClassByName((c._1: TypeName))
-          }
-          val annotation = STry {
-            rootMirror.getClassByName((c._2: TypeName))
-          }
-          println("annotation "+annotation)
-          (clazz, annotation) match {
-            case (Success(classSym), Success(annotationSym)) =>
-              classSym.addAnnotation(annotationSym)
+    def newTransformer(unit: CompilationUnit) =
+      new Transformer {
 
-              unit.warning(null, s"adding annotation ${c._2} to class ${c._1}")
-            case _ =>
-              unit.warning(null, s"ANNOTATION ADDER ERROR: ${c._1} or ${c._2} not found")
+        val annotators =
+          config.flatMap { c =>
+            val originalSym = STry {
+              try
+                rootMirror.getClassByName((c._1: TypeName))
+              catch {
+                case _: MissingRequirementError => //is not a class get member
+                  val i = c._1.lastIndexOf('.')
+                  val className = c._1.substring(0, i)
+                  val memberName = c._1.substring(i + 1)
+
+                  val cl = rootMirror.getClassByName((className: TypeName))
+                  try {
+                     getMember(cl, (memberName: TermName))  
+                  } catch {
+                    case err: Throwable =>
+                      err.printStackTrace
+                      throw err
+                  }
+                case err: Throwable =>
+                  err.printStackTrace
+                  throw err
+              }
+            }
+            val annotation = STry {
+
+                rootMirror.getClassByName((c._2: TypeName))
+            }
+            
+            val params = 
+                c._3.map(x => { //maybe a little better could be done
+                  x match {
+                    case "true" => reify(true).tree
+                    case "false" => reify(false).tree
+                    case str => reify(str.toString()).tree
+                  }
+                })
+
+            (originalSym, annotation) match {
+              case (Success(orSym), Success(annotationSym)) =>
+                //orSym.addAnnotation(annotationSym)
+                Some(orSym, annotationSym, params)
+              //unit.warning(null, s"adding annotation ${c._2} to ${c._1}")
+              case _ =>
+                None
+              //unit.warning(null, s"ANNOTATION ADDER ERROR: ${c._1} or ${c._2} not found")
+            }
           }
+
+        //probably we could avoid to use a transformer and use a traver only
+        override def transform(tree: Tree): Tree = {
+
+          try {
+            val toAnnotate = annotators.find { case (symb,_,_) => symb == tree.symbol}
+
+            val toValAnnotate = 
+              if (toAnnotate.isDefined) None
+              else annotators.find { case (symb,_,_) => 
+                symb.owner == tree.symbol.owner && (symb.nameString+" " : TermName) == tree.symbol.name}
+
+            if (toAnnotate.isDefined) {
+              tree.symbol.addAnnotation(toAnnotate.get._2, toAnnotate.get._3: _*)
+            } else if (toValAnnotate.isDefined) {
+              tree.symbol.addAnnotation(toValAnnotate.get._2, toAnnotate.get._3: _*)
+            }
+          } catch {
+            case _ : Throwable =>
+          }
+          
+          super.transform(tree)
         }
       }
-    }
   }
 }
